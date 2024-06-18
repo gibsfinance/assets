f72e62e7afe9fac9fe0f56ce4d5e577774ffa454
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-06-18 16:30:45 -0500

    Adds cors support

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,9 +2,11 @@ import * as express from 'express'
 import bodyParser from 'body-parser'
 import { router } from './routes'
 import responseTime from 'response-time'
+import cors from 'cors'
 
 export const app = express.default()
 
+app.use(cors())
 app.use(responseTime())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
