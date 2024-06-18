ca350b1a422e8de5e2795e8c09becfb9005a4daa
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-06-18 13:10:13 -0500

    Adds test tooling

--- test/list.test.ts
+++ test/list.test.ts
@@ -8,57 +8,77 @@ import { List, Provider } from 'knex/types/tables'
 import assert from 'assert'
 import * as utils from '../src/utils'
 import { zeroAddress } from 'viem'
+import { setup } from './utils.ts'
 
 test('/list', async (t) => {
-  let provider!: Provider
-  let list!: List
-  let list2!: List
+  // let provider!: Provider
+  // let list!: List
+  // let list2!: List
   t.beforeEach(async () => {
-    provider = await db.insertProvider({
-      name: 'Provider ABC',
-      key: 'provider-abc',
-    })
-    list = await db.insertList({
-      providerId: provider.providerId,
-      key: 'list-abc',
-      default: true,
-    })
-    list2 = await db.insertList({
-      providerId: provider.providerId,
-      key: 'list-abc2',
-    })
-    await db.insertNetworkFromChainId(10_001)
-    const token = await db.insertToken({
-      providedId: zeroAddress,
-      symbol: 'ETH',
-      name: 'Ether',
-      decimals: 18,
-      networkId: utils.chainIdToNetworkId(10_001),
-    })
-    await db.insertListToken({
-      providedId: token.providedId,
-      networkId: token.networkId,
-      listId: list.listId,
-    })
+    await setup()
+    // provider = await db.insertProvider({
+    //   name: 'Provider ABC',
+    //   key: 'provider-abc',
+    // })
+    // list = await db.insertList({
+    //   providerId: provider.providerId,
+    //   key: 'list-abc',
+    //   default: true,
+    // })
+    // list2 = await db.insertList({
+    //   providerId: provider.providerId,
+    //   key: 'list-abc2',
+    // })
+    // await db.insertNetworkFromChainId(10_001)
+    // const token0 = await db.insertToken({
+    //   providedId: zeroAddress,
+    //   symbol: 'ETH',
+    //   name: 'Ether',
+    //   decimals: 18,
+    //   networkId: utils.chainIdToNetworkId(10_001),
+    // })
+    // const token1 = await db.insertToken({
+    //   providedId: zeroAddress,
+    //   symbol: 'ETH',
+    //   name: 'Ether',
+    //   decimals: 18,
+    //   networkId: utils.chainIdToNetworkId(1),
+    // })
+    // await db.insertListToken({
+    //   providedId: token0.providedId,
+    //   networkId: token0.networkId,
+    //   listId: list.listId,
+    // })
+    // await db.insertListToken({
+    //   providedId: token1.providedId,
+    //   networkId: token1.networkId,
+    //   listId: list.listId,
+    // })
   })
   t.afterEach(async () => {
-    await db.getDB().from(tableNames.provider)
-      .delete()
-      .where({
-        providerId: provider.providerId,
-      })
+    await testUtils.teardown()
+    // await db.getDB().from(tableNames.provider)
+    //   .delete()
+    //   .where({
+    //     providerId: provider.providerId,
+    //   })
   })
   await t.test('/:providerKey', async (t) => {
     await t.test('/:listKey?', async (t) => {
       const res = await supertest(app).get(`/list/${provider.key}/${list.key}`)
         .expect(200)
-      // console.log(res)
+      console.log(res.body)
       assert.strictEqual(true, true)
+      await t.test('filter by chain id', async () => {
+        const res = await supertest(app).get(`/list/${provider.key}/${list.key}?chainId=10001`)
+          .expect(200)
+        console.log(res.body)
+      })
+      await t.test('filter by decimals', async () => {
+        const res = await supertest(app).get(`/list/${provider.key}/${list.key}?decimals=8`)
+          .expect(200)
+        console.log(res.body)
+      })
     })
-  })
-  await t.test('filter by chain id', async () => {
-    const res = await supertest(app).get(`/list/${provider.key}/${list.key}?chainId=10001`)
-      .expect(200)
-    console.log(res.body)
   })
 })
---
+++ test/utils.ts
@@ -0,0 +1,94 @@
+import { List, ListToken, Network, Provider, Token } from 'knex/types/tables'
+import * as db from '../src/db'
+import * as viem from 'viem'
+import _ from 'lodash'
+import { TableNames, Tx } from '../src/db/tables'
+
+const inserted: Partial<Record<TableNames, any[]>> = {
+  network: [] as Network[],
+  provider: [] as Provider[],
+  list: [] as List[],
+  token: [] as Token[],
+  list_token: [] as ListToken[],
+}
+
+export const get = (key: TableNames, index = 0) => {
+}
+
+export const teardown = async () => {
+  await db.transaction(async () => {
+
+  })
+}
+
+const insert = async <T = any>(list: T[], count: number, fn: (i: number, tx: Tx) => Promise<T>) => {
+  const len = list.length
+  await db.transaction(async (tx) => {
+    for (let i = 0; i < count; i++) {
+      list.push(await fn(i, tx))
+    }
+  })
+  return list.slice(len)
+}
+
+export const setup = async () => {
+  const providers = await insert(inserted.providers, 4, (i, tx) => (
+    db.insertProvider({
+      name: 'Provider ABC' + i,
+      key: 'provider-abc' + i,
+    }, tx)
+  ))
+  const networks = await insert(inserted.networks, 3, (i, tx) => (
+    db.insertNetworkFromChainId(i, 'test', tx)
+  ))
+  const providerToList = new Map<string, List[]>()
+  for (const [pI, provider] of Object.entries(providers)) {
+    const lists = await insert(inserted.list, 3, (i, tx) => (
+      db.insertList({
+        providerId: provider.providerId,
+        key: 'list-abc' + i,
+        default: +pI === i,
+      }, tx)
+    ))
+    providerToList.set(provider.providerId, lists)
+  }
+  const tokensUnderNetworkId = new Map<string, Token[]>()
+  for (const network of networks) {
+    const tokens = await insert(inserted.token, 5, (i, tx) => (
+      db.insertToken({
+        providedId: viem.padHex(viem.toHex(new Uint8Array([+network.chainId, i])), {
+          size: 20,
+          dir: 'left',
+        }),
+        symbol: 'ETH' + i,
+        name: 'Ether' + i,
+        decimals: 18,
+        networkId: network.networkId,
+      }, tx)
+    ))
+    tokensUnderNetworkId.set(network.networkId, tokens)
+  }
+  for (const [, lists] of Object.entries(providerToList)) {
+    for (const list of lists) {
+      for (const [networkId, tokens] of Object.entries(tokensUnderNetworkId)) {
+        for (const token of tokens) {
+          await db.insertListToken({
+            providedId: token.providedId,
+            networkId: networkId,
+            listId: list.listId,
+          })
+        }
+      }
+    }
+  }
+  // await db.insertListToken({
+  //   providedId: token0.providedId,
+  //   networkId: token0.networkId,
+  //   listId: list1.listId,
+  // })
+  // await db.insertListToken({
+  //   providedId: token1.providedId,
+  //   networkId: token1.networkId,
+  //   listId: list1.listId,
+  // })
+}
