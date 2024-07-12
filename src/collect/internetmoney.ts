67a6d27016fdfadae362d076d34949ed75956efb
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-07-12 15:41:32 -0500

    Adds miss

--- src/collect/internetmoney.ts
+++ src/collect/internetmoney.ts
@@ -13,6 +13,7 @@ const baseUrl = 'https://im-wallet.herokuapp.com/api/v1/networks'
 export const collect = async () => {
   return await utils.spinner('internetmoney', async () => {
     const json = await fetch(baseUrl).then((res): Promise<InternetMoneyNetwork[]> => res.json())
+      .then((res) => Array.isArray(res) ? res : [])
     const todos: Todo[] = []
     const encounteredChainIds = new Set<bigint>()
     let provider!: Provider
@@ -51,74 +52,74 @@ export const collect = async () => {
         } as unknown as viem.Chain
       }
       await db.insertNetworkFromChainId(chain.id)
-      ;((network) => {
-        const client = viem.createClient({
-          transport: viem.http(),
-          chain,
-        })
-        encounteredChainIds.add(BigInt(chain.id))
-        const insertAndGetNetworkList = (t: Tx) => {
-          return db.insertList(
-            {
-              providerId: provider.providerId,
-              networkId: utils.chainIdToNetworkId(chain.id),
-              key: `wallet-${chain.id}`,
-              name: `default wallet list for chain ${chain.id}`,
-              description: `the list that loads by default in the wallet for ${chain.id}`,
-            },
-            t,
-          )
-        }
-        todos.push(async () => {
-          await db.transaction(async (tx) => {
-            const networkList = await insertAndGetNetworkList(tx)
-            await db.fetchImageAndStoreForList(
-              {
-                listId: networkList.listId,
-                uri: network.icon,
-                originalUri: network.icon,
-                providerKey: provider.key,
-              },
-              tx,
-            )
+        ; ((network) => {
+          const client = viem.createClient({
+            transport: viem.http(),
+            chain,
           })
-        })
-        todos.push(
-          ...network.tokens.map((token) => async () => {
-            const address = token.address as viem.Hex
-            const [name, symbol, decimals] = await utils.erc20Read(chain, client, address)
+          encounteredChainIds.add(BigInt(chain.id))
+          const insertAndGetNetworkList = (t: Tx) => {
+            return db.insertList(
+              {
+                providerId: provider.providerId,
+                networkId: utils.chainIdToNetworkId(chain.id),
+                key: `wallet-${chain.id}`,
+                name: `default wallet list for chain ${chain.id}`,
+                description: `the list that loads by default in the wallet for ${chain.id}`,
+              },
+              t,
+            )
+          }
+          todos.push(async () => {
             await db.transaction(async (tx) => {
               const networkList = await insertAndGetNetworkList(tx)
-              const insertion = {
-                uri: token.icon,
-                originalUri: token.icon,
-                providerKey: provider.key,
-                token: {
-                  symbol,
-                  name,
-                  decimals,
-                  networkId: utils.chainIdToNetworkId(chain.id),
-                  providedId: address,
-                },
-              }
-              await db.fetchImageAndStoreForToken(
+              await db.fetchImageAndStoreForList(
                 {
-                  ...insertion,
                   listId: networkList.listId,
-                },
-                tx,
-              )
-              await db.fetchImageAndStoreForToken(
-                {
-                  ...insertion,
-                  listId: insertedList.listId,
+                  uri: network.icon,
+                  originalUri: network.icon,
+                  providerKey: provider.key,
                 },
                 tx,
               )
             })
-          }),
-        )
-      })(network)
+          })
+          todos.push(
+            ...network.tokens.map((token) => async () => {
+              const address = token.address as viem.Hex
+              const [name, symbol, decimals] = await utils.erc20Read(chain, client, address)
+              await db.transaction(async (tx) => {
+                const networkList = await insertAndGetNetworkList(tx)
+                const insertion = {
+                  uri: token.icon,
+                  originalUri: token.icon,
+                  providerKey: provider.key,
+                  token: {
+                    symbol,
+                    name,
+                    decimals,
+                    networkId: utils.chainIdToNetworkId(chain.id),
+                    providedId: address,
+                  },
+                }
+                await db.fetchImageAndStoreForToken(
+                  {
+                    ...insertion,
+                    listId: networkList.listId,
+                  },
+                  tx,
+                )
+                await db.fetchImageAndStoreForToken(
+                  {
+                    ...insertion,
+                    listId: insertedList.listId,
+                  },
+                  tx,
+                )
+              })
+            }),
+          )
+        })(network)
     }
     const limit = promiseLimit<Todo>(4)
     await limit.map(todos, async (todo) => {
