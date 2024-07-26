fd29e0654947a0349bf2f8218cb5de5a6fc3300a
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2024-07-25 23:43:16 -0500

    Adds compression

---
+++ src/collect/tokensex.ts
@@ -0,0 +1,4 @@
+// https://tokensex.link/eth-pulsechain.tokenlist.json
+// https://tokensex.link/bnb-pulsechain.tokenlist.json
+// https://tokensex.link/pulsechain-bnb.tokenlist.json
+// https://tokensex.link/pulsechain-eth.tokenlist.json
--- src/server/app.ts
+++ src/server/app.ts
@@ -3,11 +3,13 @@ import bodyParser from 'body-parser'
 import { router } from './routes'
 import responseTime from 'response-time'
 import cors from 'cors'
+import compression from 'compression'
 
 export const app = express.default()
 
 app.use(responseTime())
 app.use(cors())
+app.use(compression())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
