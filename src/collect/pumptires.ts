93406f4d64e3b7b164d1fff5495284deaae2e9c3
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2025-02-09 13:34:30 -0600

    Update to ensure that insertions finish

--- src/collect/pumptires.ts
+++ src/collect/pumptires.ts
@@ -1,11 +1,12 @@
 import _ from 'lodash'
-import { getAddress, Hex } from 'viem'
+import { concatHex, parseAbi, decodeFunctionResult, getAddress, Hex, keccak256, parseEther } from 'viem'
 import * as db from '@/db'
 import * as utils from '@/utils'
 import { pulsechain } from 'viem/chains'
 import { fetch } from '@/fetch'
 import { tableNames } from '@/db/tables'
 import { log } from '@/logger'
+import * as chains from 'viem/chains'
 
 type Creator = {
   address: Hex
@@ -113,25 +114,96 @@ export const collect = async () => {
     networkId: network.networkId,
     default: true,
   })
+  const [highMarketCapList] = await db.insertList({
+    key: 'highcap',
+    name: 'High Market Cap',
+    providerId: provider.providerId,
+    networkId: network.networkId,
+    default: false,
+  })
   const tokens = await collectTokens(list.listId)
+  const factory = '0x29eA7545DEf87022BAdc76323F373EA1e707C523'
+  const initCode = '0x5dff1ac2d132f5ac2841294c6e9fc0ebafae8d447fac7996ef21c21112f411f1'
+  const wpls = '0xA1077a294dDE1B09bB078844df40758a5D0f9a27'
   await utils.spinner('pumptires', async (l) => {
     l.incrementMax(tokens.length)
+    // const millionDollarMarketCap = parseEther('0.001') // add this constraint later
     await utils.limit.map(tokens, async (token: TokenInfo) => {
       const originalUri = `https://ipfs-pump-tires.b-cdn.net/ipfs/${token.image_cid}`
-      await db.fetchImageAndStoreForToken({
-        listId: list.listId,
-        uri: originalUri,
-        originalUri,
-        providerKey,
-        token: {
-          name: token.name,
-          symbol: token.symbol,
-          decimals: 18,
-          providedId: token.address,
-          networkId: network.networkId,
-        },
-      })
+      const [pair, token0, token1] = tokenToPair(token.address, token.address, factory, initCode)
+      const [reserves] = await Promise.all([
+        getReserves(pair), // we care about this for the price
+        db.fetchImageAndStoreForToken({
+          listId: list.listId,
+          uri: originalUri,
+          originalUri,
+          providerKey,
+          token: {
+            name: token.name,
+            symbol: token.symbol,
+            decimals: 18,
+            providedId: token.address,
+            networkId: network.networkId,
+          },
+        }),
+      ])
+      const [rt0, rt1, timestamp] = reserves
+      const wplsReserve = token0 === wpls ? rt0 : rt1
+      const tokenReserve = token1 === wpls ? rt0 : rt1
+      const oneBillion = 1_000_000_000n
+      const oneEther = 10n ** 18n
+      const oneBillionWei = oneBillion * oneEther
+      // const amount = tokenReserve / wplsReserve
+      // const price =
+      if (wplsReserve > oneBillionWei) {
+        console.log('inserting highcap token', token.address)
+        await db.fetchImageAndStoreForToken({
+          listId: list.listId,
+          uri: originalUri,
+          originalUri,
+          providerKey,
+          token: {
+            name: token.name,
+            symbol: token.symbol,
+            decimals: 18,
+            providedId: token.address,
+            networkId: network.networkId,
+          },
+        })
+      }
       l.incrementCurrent()
     })
   })
 }
+
+const client = utils.publicClient(chains.pulsechain)
+
+const univ2Abi = parseAbi([
+  'function getReserves() view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)',
+])
+
+const getReserves = async (pair: Hex) => {
+  const [result] = await client.multicall({
+    contracts: [
+      {
+        abi: univ2Abi,
+        address: pair,
+        functionName: 'getReserves',
+        args: [],
+      },
+    ],
+  })
+  if (result.status === 'failure') {
+    return [0n, 0n, 0] as const
+  }
+  return result.result
+}
+
+const tokenToPair = _.memoize((token0: Hex, token1: Hex, factory: Hex, initCodeHash: Hex) => {
+  const tl0 = token0.toLowerCase()
+  const tl1 = token1.toLowerCase()
+  const [t0, t1] = tl0 < tl1 ? [token0, token1] : [token1, token0]
+  // should check chain for these values in the future
+  const input = concatHex(['0xff', factory, keccak256(concatHex([t0, t1])), initCodeHash])
+  return [`0x${keccak256(input).slice(-40)}` as Hex, getAddress(t0), getAddress(t1)] as const
+})
