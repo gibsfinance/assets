8d1e9b58de68c1acdb63e13a1937ddbc46576c9b
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2025-02-12 10:28:04 -0600

    Refactor chain configuration and collectables initialization
    - Move chain configuration into a function in chains.ts
    - Update collectables to be a function that uses the chain configuration
    - Adjust imports and usage in related files

--- src/chains.ts
+++ src/chains.ts
@@ -8,41 +8,50 @@ import {
 } from 'viem/chains'
 import { collect } from '@/args'
 
-const { rpc1, rpc369, rpc56 } = collect()
+export default () => {
+  const { rpc1, rpc369, rpc56 } = collect()
 
-export const mainnet = {
-  ...viemMainnet,
-  rpcUrls: {
-    ...viemMainnet.rpcUrls,
-    default: {
-      ...viemMainnet.rpcUrls.default,
-      http: rpc1,
+  const mainnet = {
+    ...viemMainnet,
+    rpcUrls: {
+      ...viemMainnet.rpcUrls,
+      default: {
+        ...viemMainnet.rpcUrls.default,
+        http: rpc1,
+      },
     },
-  },
-} as Chain
-export const pulsechain = {
-  ...viemPulsechain,
-  rpcUrls: {
-    ...viemPulsechain.rpcUrls,
-    default: {
-      ...viemPulsechain.rpcUrls.default,
-      http: rpc369,
+  } as Chain
+  const pulsechain = {
+    ...viemPulsechain,
+    rpcUrls: {
+      ...viemPulsechain.rpcUrls,
+      default: {
+        ...viemPulsechain.rpcUrls.default,
+        http: rpc369,
+      },
     },
-  },
-} as Chain
-export const bsc = {
-  ...viemBSC,
-  rpcUrls: {
-    ...viemBSC.rpcUrls,
-    default: {
-      ...viemBSC.rpcUrls.default,
-      http: rpc56,
+  } as Chain
+  const bsc = {
+    ...viemBSC,
+    rpcUrls: {
+      ...viemBSC.rpcUrls,
+      default: {
+        ...viemBSC.rpcUrls.default,
+        http: rpc56,
+      },
     },
-  },
-} as Chain
-export const sepolia = {
-  ...viemSepolia,
-} as Chain
-export const pulsechainV4 = {
-  ...viemPulsechainV4,
-} as Chain
+  } as Chain
+  const sepolia = {
+    ...viemSepolia,
+  } as Chain
+  const pulsechainV4 = {
+    ...viemPulsechainV4,
+  } as Chain
+  return {
+    mainnet,
+    pulsechain,
+    bsc,
+    sepolia,
+    pulsechainV4,
+  }
+}
