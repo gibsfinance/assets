556142186820100a5071bef7df09bb704d793b88
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2025-06-10 15:14:13 -0500

    feat(server): add comment for best guess routes

--- packages/server/src/server/image/index.ts
+++ packages/server/src/server/image/index.ts
@@ -8,6 +8,6 @@ router.use('/direct/:imageHash', nextOnError(handlers.getImageByHash))
 router.use('/fallback/:order/:chainId/:address', nextOnError(handlers.getImageAndFallback))
 router.use('/:order/:chainId/:address', nextOnError(handlers.getImage(true)))
 router.use('/:chainId/:address', nextOnError(handlers.getImage(false)))
-// best guess
+// best guess routes
 router.use('/:chainId', nextOnError(handlers.bestGuessNetworkImageFromOnOnChainInfo))
 router.use('/', nextOnError(handlers.tryMultiple))
