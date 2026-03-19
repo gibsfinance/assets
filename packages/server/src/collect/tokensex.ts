ff56f8f5ae618db0ae1715335d8a3e4a1ac61f53
gibsfinancedev <162373480+gibsfinancedev@users.noreply.github.com>
2026-03-18 23:32:03 -0500

    chore: remove unused tokensex.ts stub

--- packages/server/src/collect/tokensex.ts
+++
@@ -1,4 +0,0 @@
-// https://tokensex.link/eth-pulsechain.tokenlist.json
-// https://tokensex.link/bnb-pulsechain.tokenlist.json
-// https://tokensex.link/pulsechain-bnb.tokenlist.json
-// https://tokensex.link/pulsechain-eth.tokenlist.json
